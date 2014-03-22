1952bd2226aa3c25bdfdc252427f200d2fa0963d
mebinum <mebinum@gmail.com>
2014-03-22 19:05:44 +1100

    added a whole bunch of shit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,50 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(argument) {
+
+		var onMobile = false;
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+			onMobile = true;
+		}
+		$('.player').mb_YTPlayer();
+		$('#home').css({
+			'background': 'none'
+		});
+		$('#video-volume').bind('click', function(e) {
+			e.preventDefault();
+
+			$('#YTPlayer').toggleVolume();
+		});
+
+		var pageHeight = $(window).height();
+		var pageWidth = $(window).width();
+		var navigationHeight = $('#navigation').outerHeight();
+
+		// $('#navigation').fixedonlater({
+		// 	speedDown: 250,
+		// 	speedUp: 100
+		// });
+
+		/* Scroll spy and scroll filter */
+		$('#main-menu').onePageNav({
+			currentClass: 'active',
+			changeHash: false,
+			scrollOffset: navigationHeight - 10,
+			scrollThreshold: 0.5,
+			scrollSpeed: 750,
+			filter: '',
+			easing: 'swing'
+		});
+
+		// Fullscreen Height - keep it here to avoid sticky menu bug!
+		function _fullscreen() {
+			$('.full-screen, .full-screen ul, .full-screen li').height(pageHeight);
+		}
+		_fullscreen();
+
+		$(window).resize(function() {
+			_fullscreen();
+		});
+
+		// } else {
+		// 	$('.player , #video-volume').hide();
+		// }
+});
